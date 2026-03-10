1d512b97d6ef54cef88a32b1b63cbea2b41c7f30
JunKyu <rlawnsrb298@kakao.com>
2026-03-10 14:30:58 +0900

    feat: 카카오 로그인 버튼 추가

--- frontend/WYE/app/src/main/java/com/d102/wye/presentation/auth/login/components/LoginSocialSection.kt
+++ frontend/WYE/app/src/main/java/com/d102/wye/presentation/auth/login/components/LoginSocialSection.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -39,12 +40,17 @@ fun LoginSocialSection(
 
     Spacer(modifier = Modifier.height(20.dp))
 
-    WyeKakaoButton(
-        onClick = onKakaoLoginClick,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(54.dp)
-    )
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        WyeKakaoButton(
+            onClick = onKakaoLoginClick,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(52.dp)
+        )
+    }
 }
 
 @Composable
