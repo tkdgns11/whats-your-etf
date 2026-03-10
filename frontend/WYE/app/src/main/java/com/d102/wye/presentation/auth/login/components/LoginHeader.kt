0d56988b45d5a5097ca1d27d9625cb564c5cdc1f
JunKyu <rlawnsrb298@kakao.com>
2026-03-10 10:23:13 +0900

    feat: 로고 추가 및 password visibility 크기 축소

--- frontend/WYE/app/src/main/java/com/d102/wye/presentation/auth/login/components/LoginHeader.kt
+++ frontend/WYE/app/src/main/java/com/d102/wye/presentation/auth/login/components/LoginHeader.kt
@@ -1,16 +1,29 @@
 package com.d102.wye.presentation.auth.login.components
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.d102.wye.R
+import com.d102.wye.presentation.theme.PrimaryGreen
 import com.d102.wye.presentation.theme.SurfaceWhite
 
 @Composable
@@ -23,6 +36,30 @@ fun LoginHeader(
             .height(320.dp)
             .background(SurfaceWhite)
     ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 40.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_logo),
+                contentDescription = "로고"
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Text(
+                text = "What's Your ETF",
+                style = MaterialTheme.typography.titleLarge,
+                color = PrimaryGreen,
+                fontSize = 36.sp,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+
         Canvas(
             modifier = Modifier
                 .fillMaxWidth()
