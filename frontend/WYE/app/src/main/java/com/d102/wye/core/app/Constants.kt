c35d3b119ef798389f420b77e9c7712fde010df9
JunKyu <rlawnsrb298@kakao.com>
2026-03-20 14:21:41 +0900

    feat: DataStroe에 FCM 토큰 추가

--- frontend/WYE/app/src/main/java/com/d102/wye/core/app/Constants.kt
+++ frontend/WYE/app/src/main/java/com/d102/wye/core/app/Constants.kt
@@ -13,6 +13,7 @@ object Constants {
     const val PREF_NAME = "wye_prefs"
     const val KEY_ACCESS_TOKEN = "access_token"
     const val KEY_REFRESH_TOKEN = "refresh_token"
+    const val KEY_FCM_TOKEN = "fcm_token"
     const val KEY_USER_ID = "user_id"
     const val KEY_IS_LOGGED_IN = "is_logged_in"
     const val KEY_SESSION_EXPIRED = "session_expired"
