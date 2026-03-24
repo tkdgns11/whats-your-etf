cabbd11cd2b340fa73375b6ab6bd7afdfb421869
JunKyu <rlawnsrb298@kakao.com>
2026-03-24 17:54:34 +0900

    feat: Top 10 ETF 폴링 & 새로고침 추가

--- frontend/WYE/app/src/main/java/com/d102/wye/domain/repository/EtfRepository.kt
+++ frontend/WYE/app/src/main/java/com/d102/wye/domain/repository/EtfRepository.kt
@@ -8,10 +8,11 @@ import com.d102.wye.domain.model.EtfLikeData
 import com.d102.wye.domain.model.EtfPage
 import com.d102.wye.domain.model.EtfPriceData
 import com.d102.wye.domain.model.TopVolumeEtf
+import com.d102.wye.domain.model.TopVolumeEtfSnapshot
 import kotlinx.coroutines.flow.Flow
 
 interface EtfRepository {
-    suspend fun getTopVolumeEtfs(): BaseResult<List<TopVolumeEtf>>
+    suspend fun getTopVolumeEtfs(): BaseResult<TopVolumeEtfSnapshot>
     suspend fun getEtfList(filter: EtfFilter = EtfFilter(), page: Int = 0): BaseResult<EtfPage>
     fun getLikedEtfList(): Flow<List<EtfLikeData>>
     suspend fun toggleLike(data: EtfLikeData): BaseResult<Boolean>
